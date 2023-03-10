0176f88a94a87113d6020b037d703a4a3ba0b205
Surya Teja Reddy <74102066+surya-teja-222@users.noreply.github.com>
2023-03-10 13:47:34 +0530

    fixed routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,23 @@
-import "./globals.css"
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
 
+import Head from "next/head"
 export const metadata = {
 	title: "Hotel360 - Admin Dashboard",
 	description:
 		"Hotel360 - Admin Dashboard - React, Tailwind, TypeScript, Next",
 }
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode
-}) {
+export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<html lang="en">
-			<head>
+		<>
+			<Head>
 				<meta charSet="utf-8" />
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1"
 				/>
 				<meta name="description" content={metadata.description} />
-				<title>{metadata.title}</title>
 				<meta name="og:country-name" content="INDIA" />
 				<meta name="og:locale" content="en_IN" />
 				<meta name="og:title" content={metadata.title} />
@@ -31,11 +28,10 @@ export default function RootLayout({
 				<meta property="og:type" content="website" />
 				<meta name="og:image" content="/favicon.ico" />
 				<meta name="og:description" content={metadata.description} />
-				<meta http-equiv="X-UA-Compatible" content="IE=7" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=7" />
 				<link rel="icon" href="/favicon.ico" />
-			</head>
-
-			<body>{children}</body>
-		</html>
+			</Head>
+			<Component {...pageProps} />
+		</>
 	)
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import { Html, Head, Main, NextScript } from "next/document"
+
+
+
+export default function Document() {
+	return (
+		<Html lang="en">
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
+}
